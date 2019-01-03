51f280060a8ea8b8393b90af3aaa05bcaa9f65fe
Harsh Mistry <mistryharsh28@gmail.com>
2019-01-03 19:18:35 +0530

    Removed the SECRET_KEY

--- projectfinder/settings.py
+++ projectfinder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd-m1#4178pi$cof=34ts1q^i%e8f%+omlrk63*2a88b(i0tqah'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
