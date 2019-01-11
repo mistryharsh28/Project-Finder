14f97f6fb2555227874b4a22117b51d68564aa63
Harsh Mistry <mistryharsh28@gmail.com>
2019-01-11 19:31:35 +0530

    Updated Hackathon, HackathonTeam and HackathonTeamRequest models

---
+++ project_finder_web_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from datetime import date
+
+class Hackathon(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	hackathon_desc = models.TextField(max_length=500)
+	link = models.URLField()
+	hackathon_date = models.DateField(default=date.today)
+
+	def __str__(self):
+		return self.name
+
+class HackathonTeam(models.Model):
+	name = models.CharField(max_length=50, unique=True)
+	leader = models.ForeignKey(User, on_delete=models.CASCADE)
+	current_members = models.ManyToManyField(User)
+	hackathon = models.OneToOneField(Hackathon, on_delete=models.CASCADE)
+	vacancies = models.PositiveSmallIntegerField(default=3)
+	closed = models.BooleanField(default=False)
+	cut_off_date = models.DateField(default=date.today)
+
+	def __str__(self):
+		return self.name
+
+class HackathonTeamRequest(models.Model):
+	sender = models.ForeignKey(User, on_delete=CASCADE)
+	team = models.ForeignKey(HackathonTeam, on_delete=CASCADE)
+	message = models.TextField(max_length=500)
+	create_date = models.DateField(default=date.today)
+	status_choices = (
+		('A' : 'Accepted'),
+		('P' : 'Pending'),
+		('R' : 'Rejected'),
+		)
+	status = models.CharField(max_length=1, choices=status_choices, default='P')
