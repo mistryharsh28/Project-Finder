f85dc7f6269ff1b1ad7dd227bf74f68d8df6554a
Harsh Mistry <mistryharsh28@gmail.com>
2019-02-27 14:16:48 +0530

    Add permissions

---
+++ project_finder_web_app/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+from .models import User
+
+
+class IsReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsTeacher(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+            if request.user.is_superuser:
+                return True
+            elif request.user and request.user.is_authenticated:
+                if (request.user.is_teacher):
+                    return True
+            return False
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.creator == request.user
